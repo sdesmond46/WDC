4a62c1b92d2369b218fa82d5c55fc511e41d1b01
sdesmond46 <sdesmond46@gmail.com>
2015-10-13 23:39:59 -0700

    Complete twitter code

--- public/javascripts/twitter.js
+++ public/javascripts/twitter.js
@@ -1,3 +1,14 @@
+$(document).ready(function() {
+	$("#getDataButton").click(function() {
+		var searchText = $("#searchInput").val();
+		console.log("Search text is " + searchText);
+		if (searchText.length > 0) {
+			tableau.connectionData = searchText;
+			tableau.connectionName = "Twitter Search: " + searchText;
+			tableau.submit();
+		}
+	})
+});
 
 // WDC specific stuff
 var myConnector = tableau.makeConnector();
@@ -7,15 +18,30 @@ myConnector.init = function() {
 }
 
 myConnector.getColumnHeaders = function() {
-	var fieldNames = []
-
-var fieldTypes = [
-];
-
-  tableau.headersCallback(fieldNames, fieldTypes);
+	$.getJSON("./headers", function(data) {
+		var fieldNames = [];
+		var fieldTypes = [];
+		
+		for(var i=0; i<data.length; i++) {
+			fieldNames.push(data[i][0]);
+			fieldTypes.push(data[i][1]);
+		}
+		
+		tableau.headersCallback(fieldNames, fieldTypes);
+	});
 };
 
 myConnector.getTableData = function(lastRecordToken) {
+	var queryString = tableau.connectionData;
+	var url = './tableData?q=' + encodeURI(queryString);
+	
+	console.log("Query string is '" + queryString + "'");
+	console.log("url is '" + url + "'");
+	
+	$.getJSON(url, function(data) {
+		tableau.dataCallback(data, "", false);
+	});
 };
 
 tableau.registerConnector(myConnector);
+
--- routes/twitter_server.js
+++ routes/twitter_server.js
@@ -4,10 +4,15 @@ var request = require('request');
 
 var OAuth2 = require('OAuth').OAuth2; 
 
-var clientId = process.env.APPSETTING_Square_ClientId;
-var appSecret = process.env.APPSETTING_Square_AppSecret;
+// Need a long 64 bit integer to do the addition
+var Long = require("long");
+
+var clientId = process.env.APPSETTING_Twitter_ClientId;
+var appSecret = process.env.APPSETTING_Twitter_AppSecret;
 
 var oauth2 = new OAuth2(
+  clientId,
+  appSecret, 
   'https://api.twitter.com/', null, 'oauth2/token', null);
 
 
@@ -26,6 +31,104 @@ router.get('/', function(req, res) {
 
 router.get('/accessToken', function(req, res) {
   res.send('Code is blah');
+});
+
+function getHeaders() {
+  var columns = [
+    ["text", "string"],
+    ["retweeted", "bool"],
+    ["created_at", "datetime"],
+    ["favorite_count", "int"],
+    ["id_str", "string"],
+    ["in_reply_to_screen_name", "string"],
+    ["in_reply_to_status_id_str", "string"],
+    ["lang", "string"],
+    ["retweet_count", "int"],
+    ["source", "string"]
+  ];
+  
+  return columns;
+}
+
+router.get('/headers', function(req, res) {
+  
+  res.send(getHeaders());
+});
+
+function getUrl(userId, lastId) {
+  var max_id_str = "";
+  if (!!lastId) {
+    var max_id = Long.fromString(lastId);
+    max_id = max_id.add(-1);
+    max_id_str = max_id.toString();
+  }
+  
+  
+  var url = "https://api.twitter.com/1.1/statuses/user_timeline.json?count=200&screen_name=" + userId; 
+  if (!!lastId) {
+    url += "&max_id=" + max_id_str;
+  }
+  
+  console.log("url is '" + url + "'");
+  return url;
+}
+
+function getData(userId, lastId, columns, outDataArray, doneCallback) {
+    var oauth_request_headers = { 'Authorization':  'Bearer ' + accessToken, 'Accept': 'application/json'};
+    
+    var options = {
+      method: 'GET',
+      url: getUrl(userId, lastId),
+      headers: oauth_request_headers
+    };
+                          
+                          
+  request(options, function (error, response, body) {
+    if (!error) {
+      console.log("Received Successful response");
+        var parsedData = JSON.parse(body);
+        for(var i = 0; parsedData && parsedData.length && i < parsedData.length; i++) {
+          var status = parsedData[i];
+          var rowData = {};
+          for(var j=0; j<columns.length; j++) {
+            var col = columns[j];
+            var data = status[col[0]];
+            if (col[1] == "datetime" || col[1] == "date") {
+              data = (new Date(data)).toISOString();
+            }
+            
+            rowData[col[0]] = data;
+          }
+          
+          outDataArray.push(rowData);
+        }
+        
+        if (!parsedData || parsedData.length == 0 || outDataArray.length >= 3200) {
+          console.log("Finsihed final response. NOR is " + outDataArray.length);
+          doneCallback();
+        } else {
+          console.log("Getting more data. NOR is " + outDataArray.length);
+          getData(userId, outDataArray[outDataArray.length - 1].id_str, columns, outDataArray, doneCallback);
+        }
+    } else {
+      console.log(error);
+    }
+  });
+}
+
+router.get('/tableData', function(req, res) {
+  var query = req.query.q;
+  console.log("Query was " + query);
+
+  var data = [];
+  
+  getData(query, undefined, getHeaders(), data, function() {
+    res.send(data);
+  });
+  
+  // getData('https://api.twitter.com/1.1/search/tweets.json?' + "result_type=recent&count=100&q=" + query, getHeaders(), data, function() {
+  //   res.send(data);
+  // });
 });
 
 router.get('/searchTwitter', function(req, res) {
